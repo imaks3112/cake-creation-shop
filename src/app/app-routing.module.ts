e3cc7a2411a1f820aef497b181b5e18aeca800d1
imakshaylankeshwar <akshay.lankeshwar@zylon.io>
2024-07-13 22:12:33 +0530

    implement shared compponent and move sharable  code into shared component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 import { CakeListComponent } from './components/cake-list/cake-list.component';
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './shared/login/login.component';
 import { CakeListComponent } from './components/cake-list/cake-list.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 import { NgbModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
