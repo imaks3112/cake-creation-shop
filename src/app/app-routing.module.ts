a8b1a4058911f5eb93bf10ef9779f384d6137ed6
imakshaylankeshwar <akshay.lankeshwar@zylon.io>
2024-07-16 02:05:29 +0530

    implement badge for the cart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,12 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 import { CakeListComponent } from './components/cake-list/cake-list.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './shared/login/login.component';
 
 const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent,
+  },
   {
     path: '', component: NavbarComponent, 
     children: [
