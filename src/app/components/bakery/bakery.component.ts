dbf59835fbebd4da678c87085e1bf770311ec782
imakshaylankeshwar <akshay.lankeshwar@zylon.io>
2024-07-22 12:37:55 +0530

    Shows data in ALl the tabs.

--- src/app/components/bakery/bakery.component.ts
+++ src/app/components/bakery/bakery.component.ts
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./bakery.component.scss']
 })
 export class BakeryComponent {
-
+  cakeList = [
+    {
+      id: 1,
+      name: 'Birthday cake',
+      price: 500,
+      imageurl: '../../../assets/img/cake_creation_13.jpg'
+    },
+    {
+      id: 2,
+      name: 'Birthday cake',
+      price: 4500,
+      imageurl: '../../../assets/img/cake_creation_7.jpg'
+    },
+    {
+      id: 3,
+      name: 'Birthday cake',
+      price: 350,
+      imageurl: '../../../assets/img/cake_creation_10.jpg'
+    },
+    {
+      id: 4,
+      name: 'Birthday cake',
+      price: 500,
+      imageurl: '../../../assets/img/cake_creation_11.jpg'
+    },
+    {
+      id: 5,
+      name: 'Birthday cake',
+      price: 2500,
+      imageurl: '../../../assets/img/cake_creation_6.jpg'
+    },
+    {
+      id: 3,
+      name: 'Birthday cake',
+      price: 500,
+      imageurl: '../../../assets/img/cake_creation_10.jpg'
+    },
+    {
+      id: 4,
+      name: 'Birthday cake',
+      price: 500,
+      imageurl: '../../../assets/img/cake_creation_11.jpg'
+    },
+    {
+      id: 5,
+      name: 'Birthday cake',
+      price: 500,
+      imageurl: '../../../assets/img/cake_creation_6.jpg'
+    }
+  ]
 }
